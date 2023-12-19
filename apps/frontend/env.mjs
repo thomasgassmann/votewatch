5cc65f1fd511d1b8fd0a50557a85a4bf97f0c1fc
Nick <nick@nickcrd.dev>
2023-12-19 19:31:52 +0100

    fix: Remove unnessecary test env variable

--- apps/frontend/env.mjs
+++ apps/frontend/env.mjs
@@ -4,13 +4,11 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().min(1),
-    TEST_ENV: z.string(),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   runtimeEnv: {
-    TEST_ENV: process.env.TEST_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
