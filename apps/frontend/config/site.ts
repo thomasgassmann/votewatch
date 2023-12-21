e099a8d3b2ff9a234a0571cabd216836baa0b2b6
Thomas Gassmann <tgassmann@student.ethz.ch>
2023-12-21 02:15:32 +0100

    Actually change site url
    Signed-off-by: Thomas Gassmann <tgassmann@student.ethz.ch>

--- apps/frontend/config/site.ts
+++ apps/frontend/config/site.ts
@@ -3,5 +3,5 @@ import { SiteConfig } from "../types"
 // Meta and OG data
 export const siteConfig: SiteConfig = {
   description: "Keeping an eye on politics",
-  url: "https://nickcrd.dev",
+  url: "http://mcarigiet-project-express.course-fwe-2023.isginf.ch/",
 }
