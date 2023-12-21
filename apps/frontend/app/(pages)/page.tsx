996a1a5195847211764eb7022d566e2a7f801dab
Thomas Gassmann <tgassmann@student.ethz.ch>
2023-12-21 01:06:11 +0100

    Remove unused file
    Signed-off-by: Thomas Gassmann <tgassmann@student.ethz.ch>

--- apps/frontend/app/(pages)/page.tsx
+++ apps/frontend/app/(pages)/page.tsx
@@ -26,7 +26,7 @@ export default async function HomePage() {
           <h1 className=" font-sans text-2xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
             Keep an eye on politics
           </h1>
-          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+          <p className="text-muted-foreground max-w-[42rem] leading-normal sm:text-xl sm:leading-8">
             With VoteWatch you can uncover all the lobby influences in Swiss
             Parliament and view details on specific bills and organizations.
           </p>
